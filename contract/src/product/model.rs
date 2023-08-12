9f47538ad086addbbbf373c57a44268264c149e4
Vasily Styagov <styagov.dev@gmail.com>
2023-08-12 17:20:30 +0100

    Add documentation for a product model

--- contract/src/product/model.rs
+++ contract/src/product/model.rs
@@ -6,64 +6,103 @@ use crate::common::{Duration, TokenAmount, UDecimal};
 
 pub type ProductId = String;
 
+/// The `Product` struct describes the terms of a deposit jar. It can be of Flexible or Fixed type.
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, Debug)]
 #[serde(crate = "near_sdk::serde")]
 #[cfg_attr(not(target_arch = "wasm32"), derive(PartialEq))]
 pub struct Product {
+    /// The unique identifier of the product.
     pub id: ProductId,
+
+    /// The Annual Percentage Yield (APY) associated with the product.
     pub apy: Apy,
+
+    /// The capacity boundaries of the deposit jar, specifying the minimum and maximum principal amount.
     // TODO: check that remaining balance is more than cap.min on partial withdraw
     pub cap: Cap,
+
+    /// The terms specific to the product, which can be either Flexible or Fixed.
     pub terms: Terms,
+
+    /// Describes whether a withdrawal fee is applicable and, if so, its details.
     // TODO: check that amount to withdraw is more that fee on withdraw
     pub withdrawal_fee: Option<WithdrawalFee>,
+
+    /// An optional ed25519 public key used for authorization to create a jar for this product.
     pub public_key: Option<Vec<u8>>,
 }
 
+/// The `Terms` enum describes additional terms specific to either Flexible or Fixed products.
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[serde(crate = "near_sdk::serde")]
 pub enum Terms {
+    /// Describes additional terms for Fixed products.
     Fixed(FixedProductTerms),
+
+    /// Describes additional terms for Flexible products.
     Flexible,
 }
 
+/// The `FixedProductTerms` struct contains terms specific to Fixed products.
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[serde(crate = "near_sdk::serde")]
 pub struct FixedProductTerms {
+    /// The maturity term of the jar, during which it yields interest. After this period, the user can withdraw principal
+    /// or potentially restake the jar.
     pub lockup_term: Duration,
+
+    /// Indicates whether a user can refill the jar.
     pub allows_top_up: bool,
+
+    /// Indicates whether a user can restake the jar after maturity.
     pub allows_restaking: bool,
 }
 
+/// The `WithdrawalFee` enum describes withdrawal fee details, which can be either a fixed amount or a percentage of the withdrawal.
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, Debug)]
 #[serde(crate = "near_sdk::serde")]
 #[cfg_attr(not(target_arch = "wasm32"), derive(PartialEq))]
 pub enum WithdrawalFee {
+    /// Describes a fixed amount of tokens that a user must pay as a fee on withdrawal.
     Fix(TokenAmount),
+
+    /// Describes a percentage of the withdrawal amount that a user must pay as a fee on withdrawal.
     Percent(UDecimal),
 }
 
+/// The `Apy` enum describes the Annual Percentage Yield (APY) of the product, which can be either constant or downgradable.
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, Debug)]
 #[serde(crate = "near_sdk::serde")]
 #[cfg_attr(not(target_arch = "wasm32"), derive(PartialEq))]
 pub enum Apy {
+    /// Describes a constant APY, where the interest remains the same throughout the product's term.
     Constant(UDecimal),
+
+    /// Describes a downgradable APY, where an oracle can set a penalty if a user violates the product's terms.
     Downgradable(DowngradableApy),
 }
 
+/// The `DowngradableApy` struct describes an APY that can be downgraded by an oracle.
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, Debug)]
 #[serde(crate = "near_sdk::serde")]
 #[cfg_attr(not(target_arch = "wasm32"), derive(PartialEq))]
 pub struct DowngradableApy {
+    /// The default APY value if the user meets all the terms of the product.
     pub default: UDecimal,
+
+    /// The fallback APY value if the user violates some of the terms of the product.
     pub fallback: UDecimal,
 }
 
+/// The `Cap` struct defines the capacity of a deposit jar in terms of the minimum and maximum allowed principal amounts.
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, Debug)]
 #[serde(crate = "near_sdk::serde")]
 #[cfg_attr(not(target_arch = "wasm32"), derive(PartialEq))]
 pub struct Cap {
+    /// The minimum amount of tokens that can be stored in the jar.
     pub min: TokenAmount,
+
+    /// The maximum amount of tokens that can be stored in the jar.
     pub max: TokenAmount,
 }
 
