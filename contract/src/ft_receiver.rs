8d67ecee468dd31fdd586a1db2c95cfc0b89c886
Vasily Styagov <styagov.dev@gmail.com>
2023-08-12 17:56:09 +0100

    Add documentation for fr receiver

--- contract/src/ft_receiver.rs
+++ contract/src/ft_receiver.rs
@@ -5,21 +5,34 @@ use crate::*;
 use crate::jar::model::JarTicket;
 use crate::migration::model::CeFiJar;
 
+/// The `FtMessage` enum represents various commands for actions available via transferring tokens to an account
+/// where this contract is deployed, using the payload in `ft_transfer_call`.
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 #[serde(tag = "action", content = "data")]
 #[serde(rename_all = "snake_case")]
 pub enum FtMessage {
+    /// Represents a request to create a new jar for a corresponding product.
     Stake(StakeMessage),
+
+    /// Reserved for internal service use; will be removed shortly after release.
     Migrate(Vec<CeFiJar>),
+
+    /// Represents a request to refill (top up) an existing jar using its `JarIndex`.
     TopUp(JarIndex),
 }
 
+/// The `StakeMessage` struct represents a request to create a new jar for a corresponding product.
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct StakeMessage {
+    /// Data of the `JarTicket` required for validating the request and specifying the product.
     ticket: JarTicket,
+
+    /// An optional ed25519 signature used to verify the authenticity of the request.
     signature: Option<Base64VecU8>,
+
+    /// An optional account ID representing the intended owner of the created jar.
     receiver_id: Option<AccountId>,
 }
 
