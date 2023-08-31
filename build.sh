eda86f66214a0c595e342209aa48e09280a710d3
Vladas Zakrevskis <146100@gmail.com>
2023-08-31 13:14:18 +0300

    build only sweat_jar

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -eox pipefail
 echo ">> Building contract"
 
 rustup target add wasm32-unknown-unknown
-cargo build --all --target wasm32-unknown-unknown --release
+cargo build -p sweat_jar --target wasm32-unknown-unknown --release
 
 mv ./target/wasm32-unknown-unknown/release/sweat_jar.wasm res/sweat_jar.wasm
