363a99883ab36ecacb4bd435dfe6ba0da6009e16
Vladas Zakrevskis <146100@gmail.com>
2023-08-31 13:12:41 +0300

    added workspace

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eox pipefail
 
-HOST_DIR="${HOST_DIR:-$(pwd)/..}"
+HOST_DIR="${HOST_DIR:-$(pwd)}"
 
 docker run \
      --mount type=bind,source=$HOST_DIR,target=/host \
      --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
      -t nearprotocol/contract-builder:latest-amd64 \
-     /bin/bash -c "cd /host/contract && ./build.sh"
+     /bin/bash -c "cd /host && ./build.sh"
