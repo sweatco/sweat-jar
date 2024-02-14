cd86bf4c9577caa8744f80c9bbd09ce852fe45f5
Vladas <146100@gmail.com>
2024-02-14 17:32:53 +0200

    feat: test with different data

--- integration-tests/src/migrations/claim_rounding.rs
+++ integration-tests/src/migrations/claim_rounding.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use fake::Fake;
 use integration_utils::{contract_call::set_integration_logs_enabled, misc::ToNear};
 use near_sdk::AccountId;
 use near_workspaces::types::NearToken;
@@ -15,6 +16,9 @@ use crate::{
 #[tokio::test]
 #[ignore]
 async fn migrate_to_claim_roundings() -> Result<()> {
+    use std::time::Instant;
+    let now = Instant::now();
+
     set_integration_logs_enabled(false);
 
     let jar_before_rounding = load_wasm("res/sweat_jar_before_rounding.wasm");
@@ -31,30 +35,38 @@ async fn migrate_to_claim_roundings() -> Result<()> {
 
     let jar_account = context.sweat_jar().contract.as_account().clone();
 
-    let users_count = 50;
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+
+    let users_count = 5000;
+    let jars_per_user_count = 2;
+    let total_jars = users_count * jars_per_user_count;
+
+    dbg!(total_jars);
 
     let mut accounts = Vec::with_capacity(users_count);
 
     for i in 0..users_count {
-        accounts.push(
-            context
-                .account_with_balance(&format!("user_{i}"), NearToken::from_near(1))
-                .await?
-                .to_near(),
-        );
+        accounts.push(AccountId::new_unchecked(64.fake::<String>().to_ascii_lowercase()));
     }
 
-    for accs in accounts.chunks(20) {
+    let elapsed = now.elapsed();
+    println!("Created users elapsed: {:.2?}", elapsed);
+
+    for accs in accounts.chunks(850) {
         context
             .sweat_jar()
             .bulk_create_jars(
                 accs.to_vec(),
                 RegisterProductCommand::Locked12Months12Percents.id(),
-                100_000,
-                1_000,
+                NearToken::from_near(30).as_yoctonear(),
+                jars_per_user_count as u32,
             )
             .await?;
     }
+
+    let elapsed = now.elapsed();
+    println!("Created jars elapsed: {:.2?}", elapsed);
 
     let jar_after_rounding = load_wasm("res/sweat_jar.wasm");
     let jar_after_rounding = jar_account.deploy(&jar_after_rounding).await?.into_result()?;
@@ -62,17 +74,26 @@ async fn migrate_to_claim_roundings() -> Result<()> {
         contract: &jar_after_rounding,
     };
 
+    let elapsed = now.elapsed();
+    println!("Updated contract elapsed: {:.2?}", elapsed);
+
     set_integration_logs_enabled(true);
 
     jar_after_rounding.migrate_state_to_claim_remainder().await?;
 
-    for accs in accounts.chunks(20) {
+    for accs in accounts.chunks(600) {
         jar_after_rounding
             .migrate_accounts_to_claim_remainder(accs.to_vec())
             .await?;
     }
 
-    check_jars_after_migration(accounts, jar_after_rounding).await?;
+    let elapsed = now.elapsed();
+    println!("Migrated elapsed: {:.2?}", elapsed);
+
+    // check_jars_after_migration(accounts, jar_after_rounding).await?;
+    //
+    // let elapsed = now.elapsed();
+    // println!("Check elapsed: {:.2?}", elapsed);
 
     Ok(())
 }
