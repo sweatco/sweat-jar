4cd8e26242dc1885d48f4350a9eb4d36b25c5fd5
Vladas Zakrevskis <146100@gmail.com>
2024-03-07 13:02:55 +0200

    fix: deleted unrelated tests

--- integration-tests/src/migrations/claim_rounding.rs
+++ integration-tests/src/migrations/claim_rounding.rs
@@ -2,11 +2,10 @@ use anyhow::Result;
 use fake::Fake;
 use integration_utils::{contract_call::set_integration_logs_enabled, misc::ToNear};
 use near_sdk::AccountId;
-use near_workspaces::{network::Testnet, types::NearToken, Account, Contract, Worker};
+use near_workspaces::types::NearToken;
 use sweat_jar_model::{
     api::{
-        IntegrationTestMethodsIntegration, JarApiIntegration, MigrationToClaimRemainderIntegration,
-        ProductApiIntegration, SweatJarContract,
+        IntegrationTestMethodsIntegration, JarApiIntegration, MigrationToClaimRemainderIntegration, SweatJarContract,
     },
     jar::JarView,
 };
@@ -18,40 +17,7 @@ use crate::{
     product::RegisterProductCommand,
 };
 
-async fn acc_from_file(path: &str, worker: &Worker<Testnet>) -> Result<Account> {
-    #[allow(deprecated)]
-    let home = std::env::home_dir().unwrap();
-    let path = format!("{}/.near-credentials/testnet/{path}", home.to_string_lossy());
-    let account = Account::from_file(path, &worker)?;
-    Ok(account)
-}
-
-async fn testnet_jar_contract(worker: &Worker<Testnet>) -> Result<Account> {
-    acc_from_file("v8.jar.sweatty.testnet.json", worker).await
-}
-
-async fn acc_1(worker: &Worker<Testnet>) -> Result<Account> {
-    acc_from_file("sweat_testnet_1.json", worker).await
-}
-
-async fn acc_2(worker: &Worker<Testnet>) -> Result<Account> {
-    acc_from_file("sweat_testnet_2.json", worker).await
-}
-
-async fn acc_3(worker: &Worker<Testnet>) -> Result<Account> {
-    acc_from_file("sweat_testnet_3.json", worker).await
-}
-
-fn updated_code() -> Vec<u8> {
-    #[allow(deprecated)]
-    load_wasm(&format!(
-        "{}/sweat-jar/res/sweat_jar.wasm",
-        std::env::home_dir().unwrap().to_string_lossy()
-    ))
-}
-
 #[tokio::test]
-#[ignore]
 async fn migrate_to_claim_roundings() -> Result<()> {
     use std::time::Instant;
     let now = Instant::now();
@@ -182,112 +148,5 @@ async fn migrate_to_claim_roundings() -> Result<()> {
     let elapsed = now.elapsed();
     println!("Migrated elapsed: {:.2?}", elapsed);
 
-    Ok(())
-}
-
-#[tokio::test]
-#[ignore]
-async fn check_roundings_migration() -> Result<()> {
-    let worker = near_workspaces::testnet().await?;
-
-    let jar_account = testnet_jar_contract(&worker).await?;
-
-    let jar_contract = Contract::from_secret_key(jar_account.id().clone(), jar_account.secret_key().clone(), &worker);
-    let jar_contract = SweatJarContract {
-        contract: &jar_contract,
-    };
-
-    let auto_migrate_acc = acc_1(&worker).await?;
-    let manual_migrate_acc = acc_2(&worker).await?;
-
-    assert_total_principal(&jar_contract, &manual_migrate_acc, 1511568730000000000000000).await?;
-    assert_total_principal(&jar_contract, &auto_migrate_acc, 1979104990000000000000000).await?;
-
-    let _updated_code = updated_code();
-
-    let contract = jar_account.deploy(&_updated_code).await?.into_result()?;
-    let jar_contract = SweatJarContract { contract: &contract };
-
-    jar_contract
-        .migrate_accounts_to_claim_remainder(vec![manual_migrate_acc.to_near()])
-        .await?;
-
-    assert_total_principal(&jar_contract, &manual_migrate_acc, 1511568730000000000000000).await?;
-    assert_total_principal(&jar_contract, &auto_migrate_acc, 1979104990000000000000000).await?;
-
-    Ok(())
-}
-
-#[tokio::test]
-#[ignore]
-async fn check_jar_creation_on_testnet() -> Result<()> {
-    let worker = near_workspaces::testnet().await?;
-
-    let jar_account = testnet_jar_contract(&worker).await?;
-
-    let jar_contract = Contract::from_secret_key(jar_account.id().clone(), jar_account.secret_key().clone(), &worker);
-    let jar_contract = SweatJarContract {
-        contract: &jar_contract,
-    };
-
-    let bob = acc_1(&worker).await?;
-
-    let jars = jar_contract.get_jars_for_account(bob.to_near()).await?;
-    dbg!(&jars.len());
-
-    let principal = jar_contract.get_total_principal(bob.to_near()).await?.total.0;
-    dbg!(&principal);
-
-    let products = jar_contract.get_products().await?;
-
-    dbg!(&products);
-
-    let token: near_workspaces::AccountId = "vfinal.token.sweat.testnet".to_string().try_into().unwrap();
-
-    jar_contract
-        .create_jar(&bob, "locked_1_day_100_percents".into(), 1000000000000000000, &token)
-        .await?;
-
-    let jars = jar_contract.get_jars_for_account(bob.to_near()).await?;
-    dbg!(&jars.len());
-
-    let principal = jar_contract.get_total_principal(bob.to_near()).await?.total.0;
-    dbg!(&principal);
-
-    //
-    // "locked_1_day_100_percents"
-
-    Ok(())
-}
-
-#[tokio::test]
-#[ignore]
-async fn check_user_jars() -> Result<()> {
-    let worker = near_workspaces::testnet().await?;
-
-    let jar_account = testnet_jar_contract(&worker).await?;
-
-    let jar_contract = Contract::from_secret_key(jar_account.id().clone(), jar_account.secret_key().clone(), &worker);
-    let jar_contract = SweatJarContract {
-        contract: &jar_contract,
-    };
-
-    let bob = acc_3(&worker).await?;
-
-    dbg!(jar_contract.get_jars_for_account(bob.to_near()).await?);
-    dbg!(jar_contract.get_total_interest(bob.to_near()).await?);
-    dbg!(jar_contract.get_total_principal(bob.to_near()).await?);
-
-    Ok(())
-}
-
-async fn assert_total_principal(
-    contract: &SweatJarContract<'_>,
-    account: &Account,
-    expected_principal: u128,
-) -> Result<()> {
-    let jars = contract.get_jars_for_account(account.to_near()).await?;
-    let account_principal: u128 = jars.into_iter().map(|j| j.principal.0).sum();
-    assert_eq!(account_principal, expected_principal);
     Ok(())
 }
