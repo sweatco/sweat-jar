4d2374a01e6cc99f544d7676c1803feb432dc91c
Vladas Zakrevskis <146100@gmail.com>
2024-06-06 14:45:42 +0300

    fix: proper binary

--- scripts/build-in-docker.sh
+++ scripts/build-in-docker.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -eox pipefail
 
+rm ./Cargo.lock
+
+cargo clean
+
 HOST_DIR="${HOST_DIR:-$(pwd)}"
 
 docker run \
-     --rm \
-     --mount type=bind,source=$HOST_DIR,target=/host \
-     --cap-add=SYS_PTRACE \
-     --security-opt seccomp=unconfined \
-     -t nearprotocol/contract-builder:latest-amd64 \
-     /bin/bash -c "cd /host && make build"
+    --rm \
+    --mount type=bind,source=$HOST_DIR,target=/host \
+    --cap-add=SYS_PTRACE \
+    --security-opt seccomp=unconfined \
+    -t nearprotocol/contract-builder:latest-amd64 \
+    /bin/bash -c "cd /host && make build"
