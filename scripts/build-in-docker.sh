5f7127c11b3edd06aa9a5627320aadc44ccf0f88
Vladas Zakrevskis <146100@gmail.com>
2024-06-06 14:52:36 +0300

    fix: specify platform for docker build

--- scripts/build-in-docker.sh
+++ scripts/build-in-docker.sh
@@ -10,6 +10,7 @@ HOST_DIR="${HOST_DIR:-$(pwd)}"
 docker run \
     --rm \
     --mount type=bind,source=$HOST_DIR,target=/host \
+    --platform linux/amd64 \
     --cap-add=SYS_PTRACE \
     --security-opt seccomp=unconfined \
     -t nearprotocol/contract-builder:latest-amd64 \
