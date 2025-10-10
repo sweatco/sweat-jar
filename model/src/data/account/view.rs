84f4ef68f72a77176dc90077c41c46b575ad4c02
Vasily Styagov <styagov.dev@gmail.com>
2025-10-10 18:17:20 +0100

    fix: AccountView mapping

--- model/src/data/account/view.rs
+++ model/src/data/account/view.rs
@@ -2,12 +2,11 @@ use std::collections::HashMap;
 
 use near_sdk::near;
 
+use super::Account;
 use crate::{
-    data::{jar::JarView, product::ProductId},
+    data::{account::features::Feature, jar::JarView, product::ProductId},
     AccountScore,
 };
-
-use super::Account;
 
 #[near(serializers=[json])]
 #[derive(Default, Debug, PartialEq, Clone)]
@@ -28,7 +27,7 @@ impl From<Account> for AccountView {
                 .map(|(product_id, jar)| (product_id, jar.into()))
                 .collect(),
             score: value.score,
-            is_penalty_applied: value.is_penalty_applied,
+            is_penalty_applied: !value.features.is_feature_enabled(&Feature::IncreasedApy),
         }
     }
 }
